051f5e3a2b04897b93fc685aa5fb6ce974994f56
Jeremy Li <Jeremy@Jeremys-MacBook-Air.local>
2014-04-26 09:54:24 -0700

    Initial project files.

---
+++ ToDoList/ToDoList/ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Jeremy Li on 4/26/14.
+//  Copyright (c) 2014 Jeremy Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/ToDoList/ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Jeremy Li on 4/26/14.
+//  Copyright (c) 2014 Jeremy Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
